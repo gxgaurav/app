0e1a7f5bc2d03dbc1d095b6d2e94d48c57973eb4
gxgaurav <fxgaurav002@gmail.com>
2025-01-18 23:03:27 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+if (localStorage.getItem("fx_local")) {
+  window.location.href = "index.html";
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  var url = "https://script.google.com/macros/s/AKfycbwLRS80y9lZHiNMrR4mR_GZTS27J5aycIMelHx-Ajxnrp42IxEc4g0SxcoH48GXiUI5/exec";
+
+  var data = {
+    "username": username,
+    "password": password
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(data)
+  })
+  .then(response => response.text())
+  .then(responseText => {
+    if (responseText === "success") {
+      localStorage.setItem("fx_local", username);
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid username or password!");
+    }
+  })
+  .catch(error => console.error('Error:', error));
+});
---
+++ noti.js
@@ -0,0 +1,34 @@
+const localUsername = localStorage.getItem('fx_local');
+
+async function fetchUserData(username) {
+    try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbyHiJOx7XmB6VxKG95GQT_AREMYpL9BwgiEauWheTFefsbYGUIkeVZp1wKvlivUeO7U/exec?username=' + username);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+}
+
+async function initialize() {
+    if (localUsername) {
+        const userData = await fetchUserData(localUsername);
+        if (userData && userData.match) {
+            document.getElementById('counter').innerText = userData.password || 'N/A';
+            document.getElementById('userId').innerText = userData.id || '';
+        } else {
+            alert('User not found or not authorized.');
+        }
+    } else {
+        alert('No user data found in local storage.');
+    }
+}
+
+function toggleDetails() {
+    const detailsPage = document.getElementById('details-page');
+    detailsPage.style.display = detailsPage.style.display === 'block' ? 'none' : 'block';
+}
+
+window.onload = initialize;
+
+document.getElementById('counter').addEventListener('click', toggleDetails);
